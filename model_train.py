f9cfc03289d31d975d973041dbbcd16c9a7caef3
Xiaojia1234 <1399843193@qq.com>
2020-07-10 10:25:57 +0800

    modeltrain

--- model_train.py
+++ model_train.py
@@ -5,6 +5,7 @@
 
 '''
 使用/修改/注释：@Amy
+    第二个程序模块
     数据预处理之后，利用Bert提取文档的特征，每个文档的填充长度为100，对应一个768维的向量；
     然后用Keras创建DNN来进行模型训练；
     然后对测试集进行验证，并保存模型文件，便于后续的模型预测使用。
@@ -26,36 +27,36 @@ from bert.extract_feature import BertVector
 
 # 读取文件并进行转换
 bert_model = BertVector(pooling_strategy="REDUCE_MEAN", max_seq_len=100)
-print('begin encoding')
-f = lambda text: bert_model.encode([text])["encodes"][0]
-train_df['x'] = train_df['text'].apply(f)
-test_df['x'] = test_df['text'].apply(f)
-print('end encoding')
-
-x_train = np.array([vec for vec in train_df['x']])
-x_test = np.array([vec for vec in test_df['x']])
-y_train = np.array([vec for vec in train_df['label']])
-y_test = np.array([vec for vec in test_df['label']])
-print('x_train: ', x_train.shape)
-
-# Convert class vectors to binary class matrices.
-num_classes = 2
-y_train = to_categorical(y_train, num_classes)
-y_test = to_categorical(y_test, num_classes)
-
-# 创建模型
-x_in = Input(shape=(768, ))
-x_out = Dense(32, activation="relu")(x_in)
-x_out = BatchNormalization()(x_out)
-x_out = Dense(num_classes, activation="softmax")(x_out)
-model = Model(inputs=x_in, outputs=x_out)
-print(model.summary())
-
-model.compile(loss='categorical_crossentropy',
-              optimizer=Adam(),
-              metrics=['accuracy'])
-
-# 模型训练以及评估
-model.fit(x_train, y_train, batch_size=8, epochs=20)
-model.save('visit_classify.h5')
-print(model.evaluate(x_test, y_test))
+# print('begin encoding')
+# f = lambda text: bert_model.encode([text])["encodes"][0]
+# train_df['x'] = train_df['text'].apply(f)
+# test_df['x'] = test_df['text'].apply(f)
+# print('end encoding')
+#
+# x_train = np.array([vec for vec in train_df['x']])
+# x_test = np.array([vec for vec in test_df['x']])
+# y_train = np.array([vec for vec in train_df['label']])
+# y_test = np.array([vec for vec in test_df['label']])
+# print('x_train: ', x_train.shape)
+#
+# # Convert class vectors to binary class matrices.
+# num_classes = 2
+# y_train = to_categorical(y_train, num_classes)
+# y_test = to_categorical(y_test, num_classes)
+#
+# # 创建模型
+# x_in = Input(shape=(768, ))
+# x_out = Dense(32, activation="relu")(x_in)
+# x_out = BatchNormalization()(x_out)
+# x_out = Dense(num_classes, activation="softmax")(x_out)
+# model = Model(inputs=x_in, outputs=x_out)
+# print(model.summary())
+#
+# model.compile(loss='categorical_crossentropy',
+#               optimizer=Adam(),
+#               metrics=['accuracy'])
+#
+# # 模型训练以及评估
+# model.fit(x_train, y_train, batch_size=8, epochs=20)
+# model.save('visit_classify.h5')
+# print(model.evaluate(x_test, y_test))
