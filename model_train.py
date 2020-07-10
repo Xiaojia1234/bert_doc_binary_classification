a6db1c6a8592731dc20e1b25cb1dadef2c57563f
Xiaojia1234 <1399843193@qq.com>
2020-07-10 11:38:22 +0800

    modeltrain

--- model_train.py
+++ model_train.py
@@ -17,6 +17,9 @@ import os
 # 使用GPU训练
 # os.environ["CUDA_VISIBLE_DEVICES"] = "4,5,6,7,8"
 
+import warnings
+warnings.filterwarnings("ignore")
+
 import numpy as np
 from load_data import train_df, test_df
 from keras.utils import to_categorical
@@ -25,24 +28,33 @@ from keras.optimizers import Adam
 from keras.layers import Input, BatchNormalization, Dense
 from bert.extract_feature import BertVector
 
+
+
 # 读取文件并进行转换
 bert_model = BertVector(pooling_strategy="REDUCE_MEAN", max_seq_len=100)
-# print('begin encoding')
-# f = lambda text: bert_model.encode([text])["encodes"][0]
-# train_df['x'] = train_df['text'].apply(f)
-# test_df['x'] = test_df['text'].apply(f)
-# print('end encoding')
+print('begin encoding')
+f = lambda text: bert_model.encode([text])["encodes"][0]
+train_df['x'] = train_df['text'].apply(f)
+print('train_df[\'x\']:',len(train_df['x']),'train_df[\'x\'][0].shape',train_df['x'][0].shape,'\n',train_df['x'][:5])
+test_df['x'] = test_df['text'].apply(f)
+print('test_df[\'x\']:',len(test_df['x']),'test_df[\'x\'][0].shape()',test_df['x'][0].shape,'\n',test_df['x'][:5])
+print('end encoding')
 #
-# x_train = np.array([vec for vec in train_df['x']])
-# x_test = np.array([vec for vec in test_df['x']])
-# y_train = np.array([vec for vec in train_df['label']])
-# y_test = np.array([vec for vec in test_df['label']])
-# print('x_train: ', x_train.shape)
+x_train = np.array([vec for vec in train_df['x']])
+x_test = np.array([vec for vec in test_df['x']])
+y_train = np.array([vec for vec in train_df['label']])
+y_test = np.array([vec for vec in test_df['label']])
+print('x_train: ', x_train.shape)
+print('x_test: ', x_test.shape)
+
+# Convert class vectors to binary class matrices.
 #
-# # Convert class vectors to binary class matrices.
-# num_classes = 2
-# y_train = to_categorical(y_train, num_classes)
-# y_test = to_categorical(y_test, num_classes)
+num_classes = 2
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
+print('y_train : ', y_train .shape)
+print('y_test: ', y_test.shape)
+
 #
 # # 创建模型
 # x_in = Input(shape=(768, ))
@@ -59,4 +71,4 @@ bert_model = BertVector(pooling_strategy="REDUCE_MEAN", max_seq_len=100)
 # # 模型训练以及评估
 # model.fit(x_train, y_train, batch_size=8, epochs=20)
 # model.save('visit_classify.h5')
-# print(model.evaluate(x_test, y_test))
+# # print(model.evaluate(x_test, y_test))
